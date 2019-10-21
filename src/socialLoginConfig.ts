47d65aca5c58bf90d67b65cef31559cf793afbbf
Siiri Saar <s.saar@axinom.com>
2019-10-21 21:05:15 +0300

    Eliminated env variable from socialLoginConfig and replaced it with hardcoded string

--- src/socialLoginConfig.ts
+++ src/socialLoginConfig.ts
@@ -8,8 +8,8 @@ import {
     let config = new AuthServiceConfig([
       {
         id: GoogleLoginProvider.PROVIDER_ID,
-        provider: new GoogleLoginProvider(nprocess.env.GOOGLE_CLIENT_ID)
-        //"678019970011-7611nkabitnlnku9i9c9924bboop71vc.apps.googleusercontent.com"
+        provider: new GoogleLoginProvider("678019970011-7611nkabitnlnku9i9c9924bboop71vc.apps.googleusercontent.com")
+        //
       }
     ]);
   
