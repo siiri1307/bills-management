67b71334bd3c914ee9d3871c7094d89a4a30ff70
Siiri Saar <s.saar@axinom.com>
2019-10-21 22:26:03 +0300

    Refactored some unnnecessary characters

--- src/socialLoginConfig.ts
+++ src/socialLoginConfig.ts
@@ -9,7 +9,6 @@ import {
       {
         id: GoogleLoginProvider.PROVIDER_ID,
         provider: new GoogleLoginProvider("678019970011-7611nkabitnlnku9i9c9924bboop71vc.apps.googleusercontent.com")
-        //
       }
     ]);
   
