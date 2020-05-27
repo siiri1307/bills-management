d50805beb7969d61e7250f9c491e0426d6cf7669
Siiri Saar <s.saar@axinom.com>
2020-05-27 22:23:51 +0300

    Changed GoogleAuth service endpoint spelling

--- src/app/google-auth/google-auth.service.ts
+++ src/app/google-auth/google-auth.service.ts
@@ -29,7 +29,7 @@ export class GoogleAuthService {
 
   baseUrl = environment.baseUrl;
 
-  private serviceUrl: string = this.baseUrl + "/Authenticate";
+  private serviceUrl: string = this.baseUrl + "/authenticate";
 
   constructor(
     private authService: AuthService,
